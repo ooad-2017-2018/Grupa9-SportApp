55810c7af987de57d64158c16669c035dabdcf4b
Amir Muminović <amuminovic3@etf.unsa.ba>
2018-05-27 21:28:08 +0200

    Dodani Kontroleri

---
+++ Projekat/Playoff/PlayOffWeb/WebApplication1/Controllers/OOADProsliTimovisController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OOADProsliTimovisController : Controller
+    {
+        private PlayoffM db = new PlayoffM();
+
+        // GET: OOADProsliTimovis
+        public ActionResult Index()
+        {
+            var oOADProsliTimovis = db.OOADProsliTimovis.Include(o => o.OOADKorisnici);
+            return View(oOADProsliTimovis.ToList());
+        }
+
+        // GET: OOADProsliTimovis/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADProsliTimovi oOADProsliTimovi = db.OOADProsliTimovis.Find(id);
+            if (oOADProsliTimovi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oOADProsliTimovi);
+        }
+
+        // GET: OOADProsliTimovis/Create
+        public ActionResult Create()
+        {
+            ViewBag.Korisnik = new SelectList(db.OOADKorisnicis, "ID", "Username");
+            return View();
+        }
+
+        // POST: OOADProsliTimovis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Korisnik,Naziv,datumprestanka")] OOADProsliTimovi oOADProsliTimovi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OOADProsliTimovis.Add(oOADProsliTimovi);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Korisnik = new SelectList(db.OOADKorisnicis, "ID", "Username", oOADProsliTimovi.Korisnik);
+            return View(oOADProsliTimovi);
+        }
+
+        // GET: OOADProsliTimovis/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADProsliTimovi oOADProsliTimovi = db.OOADProsliTimovis.Find(id);
+            if (oOADProsliTimovi == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Korisnik = new SelectList(db.OOADKorisnicis, "ID", "Username", oOADProsliTimovi.Korisnik);
+            return View(oOADProsliTimovi);
+        }
+
+        // POST: OOADProsliTimovis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Korisnik,Naziv,datumprestanka")] OOADProsliTimovi oOADProsliTimovi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(oOADProsliTimovi).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Korisnik = new SelectList(db.OOADKorisnicis, "ID", "Username", oOADProsliTimovi.Korisnik);
+            return View(oOADProsliTimovi);
+        }
+
+        // GET: OOADProsliTimovis/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADProsliTimovi oOADProsliTimovi = db.OOADProsliTimovis.Find(id);
+            if (oOADProsliTimovi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oOADProsliTimovi);
+        }
+
+        // POST: OOADProsliTimovis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OOADProsliTimovi oOADProsliTimovi = db.OOADProsliTimovis.Find(id);
+            db.OOADProsliTimovis.Remove(oOADProsliTimovi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Projekat/Playoff/PlayOffWeb/WebApplication1/Controllers/OOADReviewsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OOADReviewsController : Controller
+    {
+        private PlayoffM db = new PlayoffM();
+
+        // GET: OOADReviews
+        public ActionResult Index()
+        {
+            var oOADReviews = db.OOADReviews.Include(o => o.OOADTimovi);
+            return View(oOADReviews.ToList());
+        }
+
+        // GET: OOADReviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADReview oOADReview = db.OOADReviews.Find(id);
+            if (oOADReview == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oOADReview);
+        }
+
+        // GET: OOADReviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.TIM = new SelectList(db.OOADTimovis, "ID", "Ime");
+            return View();
+        }
+
+        // POST: OOADReviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,komentar,ocjena,TIM")] OOADReview oOADReview)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OOADReviews.Add(oOADReview);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TIM = new SelectList(db.OOADTimovis, "ID", "Ime", oOADReview.TIM);
+            return View(oOADReview);
+        }
+
+        // GET: OOADReviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADReview oOADReview = db.OOADReviews.Find(id);
+            if (oOADReview == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TIM = new SelectList(db.OOADTimovis, "ID", "Ime", oOADReview.TIM);
+            return View(oOADReview);
+        }
+
+        // POST: OOADReviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,komentar,ocjena,TIM")] OOADReview oOADReview)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(oOADReview).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TIM = new SelectList(db.OOADTimovis, "ID", "Ime", oOADReview.TIM);
+            return View(oOADReview);
+        }
+
+        // GET: OOADReviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OOADReview oOADReview = db.OOADReviews.Find(id);
+            if (oOADReview == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oOADReview);
+        }
+
+        // POST: OOADReviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OOADReview oOADReview = db.OOADReviews.Find(id);
+            db.OOADReviews.Remove(oOADReview);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
