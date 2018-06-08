f5d2f82d2694345d4d52559a18119152363a6553
InfernalWraith <haris.s.salman@gmail.com>
2018-06-08 22:03:55 +0200

    Changelog: 	* Dodana forma za notifikacije sa potpunom funkcionalnosti 	* Dodana funkcionanlost za slanje i primanje poruka 	* Povezana funkcionalost za slanje poruka sa ManageTeam formom 	* Povezana forma Teams sa bazom podataka 	* Kod očišćen i uređeni komentari, refaktoring čitljivosti

---
+++ Projekat/Playoff/Playoff/PorukaManage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Playoff.Classes;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Playoff {
+    /// <summary>
+    /// A form used for sending messages to other users.
+    /// </summary>
+    public sealed partial class PorukaManage : Page {
+        public PorukaManage() {
+            InitializeComponent();
+        }
+
+        public void Posalji_Click(object sender, RoutedEventArgs e) {
+            try {
+                Baza.PosaljiPoruku(tbPrimaoc.Text, tbPoruka.Text);
+                Registration.Poruka("Poruka uspješno poslana!", "Uspjeh");
+            } catch {
+                Registration.Poruka("Taj korisnik ne postoji!", "Greška");
+            }
+        }
+
+        public void Prekini_Click(object sender, RoutedEventArgs e) {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(ManageTeam), e);
+        }
+    }
+}
